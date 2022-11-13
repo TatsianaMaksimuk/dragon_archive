4bbb96bb88c07c4f80547d0d3fe7f7daa01cf570
Tatsiana Maksimuk <okeanoka@gmail.com>
2022-11-12 19:48:50 -0800

    Dragon basic routes and findByName route added

---
+++ src/main/java/com/careerdevs/dragon_archive/controllers/DragonController.java
@@ -0,0 +1,96 @@
+package com.careerdevs.dragon_archive.controllers;
+
+
+import com.careerdevs.dragon_archive.models.Dragon;
+import com.careerdevs.dragon_archive.repositories.DragonsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/dragons")
+public class DragonController {
+
+    @Autowired
+    private DragonsRepository dragonsRepository;
+
+
+    @PostMapping
+    public ResponseEntity<?> addOneDragonToDB(@RequestBody Dragon newDragonData) {
+        try {
+            Dragon addedDragon = dragonsRepository.save(newDragonData);
+            return new ResponseEntity<>(addedDragon, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<?> getAllDragonsFromDB() { //ResponseEntity is for communication between server and client, I want to return dragon but not sure if it's gonna be dragon or list of dragons or exception message
+        List<Dragon> allDragons = dragonsRepository.findAll();
+        return new ResponseEntity<>(dragonsRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getDragonById(@PathVariable int id) {
+        Optional<Dragon> requestedDragon = dragonsRepository.findById(id); //optional is for db (I try to find in db but if I don't optional will have that info (id is null f.e.)
+        if (requestedDragon.isEmpty()) {
+            return new ResponseEntity<>("Dragon not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(requestedDragon.get(), HttpStatus.OK);
+    }
+
+    @PostMapping("/{id}")
+    public ResponseEntity<?> postOneDragonById(@PathVariable int id, @RequestBody Dragon newDragonData) {
+        Dragon requestedDragon = dragonsRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        if (!newDragonData.getName().equals("")) {
+            requestedDragon.setName(newDragonData.getName());
+        }
+        if (!newDragonData.getAlias().equals("")) {
+            requestedDragon.setAlias(newDragonData.getAlias());
+        }
+        if (!newDragonData.getSex().equals("")) {
+            requestedDragon.setSex(newDragonData.getSex());
+        }
+        if (!newDragonData.getAppearance().equals("")) {
+            requestedDragon.setAppearance(newDragonData.getAppearance());
+        }
+        if (newDragonData.getIsAlive() != requestedDragon.getIsAlive()) {
+            requestedDragon.setIsAlive(newDragonData.getIsAlive()); ///??
+        }
+        if (!newDragonData.getDateOfBirth().equals("")) {
+            requestedDragon.setDateOfBirth(newDragonData.getDateOfBirth());
+        }
+        if (!newDragonData.getPlaceOfBirth().equals("")) {
+            requestedDragon.setPlaceOfBirth(newDragonData.getPlaceOfBirth());
+        }
+        if (!newDragonData.getDateOfDeath().equals("")) {
+            requestedDragon.setDateOfDeath(newDragonData.getDateOfDeath());
+        }
+        if (!newDragonData.getPlaceOfDeath().equals("")) {
+            requestedDragon.setPlaceOfDeath(newDragonData.getPlaceOfDeath());
+        }
+        return ResponseEntity.ok(dragonsRepository.save(requestedDragon));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteDragonById(@PathVariable int id){
+        Dragon requestedDragon = dragonsRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        dragonsRepository.deleteById(id);
+        return ResponseEntity.ok(requestedDragon);
+    }
+
+
+    @GetMapping("/name/{name}")
+    public ResponseEntity<?> findDragonByName(@PathVariable String name){
+        Dragon requestedDragon = dragonsRepository.findDragonByName(name).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return new ResponseEntity<>(requestedDragon, HttpStatus.OK);
+    }
+
+}
--- src/main/java/com/careerdevs/dragon_archive/repositories/DragonsRepository.java
+++ src/main/java/com/careerdevs/dragon_archive/repositories/DragonsRepository.java
@@ -3,6 +3,9 @@ package com.careerdevs.dragon_archive.repositories;
 import com.careerdevs.dragon_archive.models.Dragon;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface DragonsRepository extends JpaRepository <Dragon, Integer> {
 
+    Optional<Dragon> findDragonByName(String name);
 }
